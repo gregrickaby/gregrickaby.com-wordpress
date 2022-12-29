773750d27dcdcb4977886f9a33734a03e99bb53f
Greg Rickaby <gregrickaby@gmail.com>
2022-12-28 21:53:10 -0600

    enqueue parent styles

--- themes/child-theme/functions.php
+++ themes/child-theme/functions.php
@@ -4,3 +4,18 @@
  *
  *  @package Child Theme
  */
+
+namespace ChildTheme;
+
+/**
+ * Enqueue parent theme styles.
+ */
+function enqueue_parent_styles() {
+	\wp_enqueue_style(
+		'child-style',
+		get_stylesheet_uri(),
+		[ 'seedlet' ],
+		wp_get_theme()->get( 'Version' )
+	);
+}
+add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\enqueue_parent_styles' );
