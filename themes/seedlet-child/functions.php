c48bf3a7f77f904528353e5d926f2f3bde79f607
Greg Rickaby <gregrickaby@gmail.com>
2023-01-03 15:59:07 -0600

    namespace

--- themes/seedlet-child/functions.php
+++ themes/seedlet-child/functions.php
@@ -14,9 +14,9 @@ namespace SeedletChild;
 function child_scripts() {
 	\wp_enqueue_style(
 		'seedlet-child-styles',
-		get_stylesheet_uri(),
+		\get_stylesheet_uri(),
 		[ 'seedlet-style' ],
-		wp_get_theme()->get( 'Version' )
+		\wp_get_theme()->get( 'Version' )
 	);
 }
 add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\child_scripts' );
