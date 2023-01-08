ca1557c0969e9c9d1b2b231abb05d136d8e2df7b
Greg Rickaby <gregrickaby@gmail.com>
2023-01-08 16:14:49 -0600

    adjust mimes

--- mu-plugins/must-use.php
+++ mu-plugins/must-use.php
@@ -6,14 +6,8 @@ add_filter( 'big_image_size_threshold', '__return_false' );
  * Allow additional mime types on upload.
  */
 function grd_additional_mime_types( $mimes ) {
-   $mimes['svg'] = 'image/svg+xml';
-   $mimes['webp'] = 'image/webp';
-   $mimes['heic'] = 'image/heic';
-   $mimes['heif'] = 'image/heif';
-   $mimes['heics'] = 'image/heic-sequence';
-   $mimes['heifs'] = 'image/heif-sequence';
-   $mimes['avif'] = 'image/avif';
-   $mimes['avis'] = 'image/avif-sequence';
+    $mimes['svg']  = 'image/svg+xml';
+    $mimes['avif'] = 'image/avif';
     return $mimes;
 }
 add_filter( 'upload_mimes', 'grd_additional_mime_types' );
