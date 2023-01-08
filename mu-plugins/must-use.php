299e41f50b3403ecf1db7c929a7ee6a6c69984e5
Greg Rickaby <gregrickaby@gmail.com>
2023-01-08 11:01:03 -0600

    all additional mime types

--- mu-plugins/must-use.php
+++ mu-plugins/must-use.php
@@ -2,3 +2,18 @@
 /** Disable image size threshold. */
 add_filter( 'big_image_size_threshold', '__return_false' );
 
+/**
+ * Allow additional mime types on upload.
+ */
+function grd_additional_mime_types( $mimes ) {
+   $mimes['svg'] = 'image/svg+xml';
+   $mimes['webp'] = 'image/webp';
+   $mimes['heic'] = 'image/heic';
+   $mimes['heif'] = 'image/heif';
+   $mimes['heics'] = 'image/heic-sequence';
+   $mimes['heifs'] = 'image/heif-sequence';
+   $mimes['avif'] = 'image/avif';
+   $mimes['avis'] = 'image/avif-sequence';
+    return $mimes;
+}
+add_filter( 'upload_mimes', 'grd_additional_mime_types' );
