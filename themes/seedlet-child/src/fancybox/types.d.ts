066b0c890e0bca48f87af15fcb185aad2bc3bdd1
Greg Rickaby <gregrickaby@gmail.com>
2023-01-18 22:01:16 -0600

    define slide

---
+++ themes/seedlet-child/src/fancybox/types.d.ts
@@ -0,0 +1,18 @@
+export interface Slide {
+  $thumb: {
+    origTarget: {
+      alt: string
+      title: string
+    }
+  }
+  $trigger: {
+    origTarget: {
+      alt: string
+      title: string
+    }
+  }
+  caption: string
+  src: string
+  thumb: string
+  type: string
+}
