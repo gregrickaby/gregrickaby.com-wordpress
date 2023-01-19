6a6398eaecd976ee2695334d8161c41aea97454b
Greg Rickaby <gregrickaby@gmail.com>
2023-01-18 21:59:30 -0600

    hello typescript

--- themes/seedlet-child/.eslintrc.js
+++ themes/seedlet-child/.eslintrc.js
@@ -1,3 +1,15 @@
 module.exports = {
-  extends: ['plugin:@wordpress/eslint-plugin/recommended']
+  extends: [
+    'plugin:@wordpress/eslint-plugin/esnext',
+    'plugin:@typescript-eslint/recommended',
+    'prettier'
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  root: true,
+  env: {
+    browser: true,
+    es6: true,
+    jquery: true
+  },
 }
