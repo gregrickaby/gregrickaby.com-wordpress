b1859ad5edd5042b247e605cf33d71481b21b3e3
Greg Rickaby <gregrickaby@gmail.com>
2023-01-18 20:55:26 -0600

    enqueue assets. remove jetpack stuffs.

--- themes/seedlet-child/functions.php
+++ themes/seedlet-child/functions.php
@@ -12,14 +12,22 @@ namespace SeedletChild;
  * Enqueue child styles and scripts.
  */
 function child_scripts() {
+
+	$asset_file = require \get_stylesheet_directory() . '/build/index.asset.php';
+
 	\wp_enqueue_style(
 		'seedlet-child-styles',
-		\get_stylesheet_uri(),
+		\get_stylesheet_directory_uri() . '/build/index.css',
 		[ 'seedlet-style' ],
-		\wp_get_theme()->get( 'Version' )
+		$asset_file['version']
+	);
+
+	\wp_enqueue_script(
+		'seedlet-child-scripts',
+		\get_stylesheet_directory_uri() . '/build/index.js',
+		[],
+		$asset_file['version'],
+		true
 	);
 }
 add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\child_scripts' );
-
-// Enable carousel stats.
-add_filter( 'jetpack_enable_carousel_stats', '__return_true' );
