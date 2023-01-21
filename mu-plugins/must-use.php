935b8efe65d9e18c2acb5d2aca35ed263c8fea7c
Greg Rickaby <gregrickaby@gmail.com>
2023-01-21 11:08:13 -0600

    add plugin name. add custom field meta box.

--- mu-plugins/must-use.php
+++ mu-plugins/must-use.php
@@ -1,5 +1,15 @@
 <?php
-/** Disable image size threshold. */
+/**
+ * Plugin Name: Custom Must Use
+ * Description: This plugin contains code that is required for gregrickaby.com.
+ * Version: 1.0.0
+ * Author: Greg Rickaby
+ * Author URI: https://gregrickaby.com
+ */
+
+/**
+ * Disable image size threshold.
+ */
 add_filter( 'big_image_size_threshold', '__return_false' );
 
 /**
@@ -11,3 +21,8 @@ function grd_additional_mime_types( $mimes ) {
     return $mimes;
 }
 add_filter( 'upload_mimes', 'grd_additional_mime_types' );
+
+/**
+ * Don't remove the custom field meta box.
+ */
+add_filter('acf/settings/remove_wp_meta_box', '__return_false');
