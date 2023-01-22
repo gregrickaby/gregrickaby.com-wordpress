d448fb404470e34e11215c821ab4213540eff752
Greg Rickaby <gregrickaby@gmail.com>
2023-01-22 13:09:11 -0600

    configure Fancybox

--- plugins/grd-acf-blocks/src/blocks/gallery/gallery.ts
+++ plugins/grd-acf-blocks/src/blocks/gallery/gallery.ts
@@ -1,2 +1,25 @@
-import './fancybox/index'
+import {Fancybox} from '@fancyapps/ui'
+import '@fancyapps/ui/dist/fancybox.css'
+import {disableRightClick} from './fancybox/utils'
 import './gallery.css'
+
+/**
+ * Init Fancybox.
+ *
+ * @see https://fancyapps.com/docs/ui/fancybox/options
+ * @see https://fancyapps.com/docs/ui/fancybox/plugins/toolbar
+ */
+Fancybox.bind('[data-fancybox]', {
+  animated: false,
+  groupAll: true,
+  infinite: false,
+  preload: 3,
+  Toolbar: {
+    display: ['zoom', 'slideshow', 'fullscreen', 'download', 'thumbs', 'close']
+  },
+  on: {
+    load: () => {
+      disableRightClick()
+    }
+  }
+})
