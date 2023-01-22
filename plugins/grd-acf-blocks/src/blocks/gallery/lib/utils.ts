110d25472e5855d9da4382232fd677a1a594f7f5
Greg Rickaby <gregrickaby@gmail.com>
2023-01-22 13:42:15 -0600

    spelling

--- plugins/grd-acf-blocks/src/blocks/gallery/lib/utils.ts
+++ plugins/grd-acf-blocks/src/blocks/gallery/lib/utils.ts
@@ -4,9 +4,9 @@
  * Why? Because we're displaying a lower-quality
  * WebP in the lightbox. If a user right-clicks
  * and saves the WebP, they're OS may not support
- * viewing it offlint. Additionally, if a user
- * tries to upload the WebP to a print service,
- * I've learned that big-box stores don't support it.
+ * viewing it offline. Additionally, if a user
+ * tries to upload that WebP to a print service,
+ * chances are the big-box stores won't support it.
  *
  * Alternatively, the user can click the download button
  * to get the full-quality JPG.
