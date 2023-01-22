d61dd9ef9c0b23d1eb2c7077cb6c0688608ca630
Greg Rickaby <gregrickaby@gmail.com>
2023-01-22 13:08:35 -0600

    add explanation (don't be evil!) and remove unused import.

--- plugins/grd-acf-blocks/src/blocks/gallery/fancybox/utils.ts
+++ plugins/grd-acf-blocks/src/blocks/gallery/fancybox/utils.ts
@@ -1,10 +1,15 @@
-import {Slide} from './types'
-
 /**
  * Disable right-click on main image.
  *
- * A user can still use the download
- * button to get a better quality JPG.
+ * Why? Because we're displaying a lower-quality
+ * WebP in the lightbox. If a user right-clicks
+ * and saves the WebP, they're OS may not support
+ * viewing it offlint. Additionally, if a user
+ * tries to upload the WebP to a print service,
+ * I've learned that big-box stores don't support it.
+ *
+ * Alternatively, the user can click the download button
+ * to get the full-quality JPG.
  */
 export function disableRightClick(): void {
   document.addEventListener(
