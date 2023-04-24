a4f5902ae7d46f880ca6a0bace920f8e3114e931
Greg Rickaby <gregrickaby@gmail.com>
2023-04-24 13:14:18 -0500

    reorder functions. add comments.

--- plugins/grd-acf-blocks/src/blocks/gallery/gallery.ts
+++ plugins/grd-acf-blocks/src/blocks/gallery/gallery.ts
@@ -3,10 +3,45 @@ import Masonry from 'masonry-layout'
 import imagesLoaded from 'imagesloaded'
 import '@fancyapps/ui/dist/fancybox.css'
 import {disableRightClick} from './lib/utils'
-import './gallery.css'
+import './gallery.scss'
 
-// Wait for the DOM to be ready.
+/**
+ * Wait for the the initial HTML document to be completely loaded.
+ *
+ * @see https://developer.mozilla.org/en-US/docs/Web/API/Document/DOMContentLoaded_event
+ */
 document.addEventListener('DOMContentLoaded', () => {
+  // Get the ACF Block Gallery grid.
+  const gallery = document.querySelector('.grd-acf-block-grid')
+
+  // If there is no gallery, return.
+  if (!gallery) {
+    return
+  }
+
+  /**
+   * Init imagesLoaded.
+   *
+   * @see https://imagesloaded.desandro.com/
+   */
+  const imagesLoadedInstance = imagesLoaded(gallery)
+
+  /**
+   * Init Masonry.
+   *
+   * @see https://masonry.desandro.com/options.html
+   */
+  const masonryGrid = new Masonry(gallery, {
+    itemSelector: '.grd-acf-block__image',
+    columnWidth: '.grd-acf-block-grid__sizer',
+    percentPosition: true
+  })
+
+  // When images have all loaded, init Masonry layout.
+  imagesLoadedInstance.on('done', () => {
+    masonryGrid.layout()
+  })
+
   /**
    * Init Fancybox.
    *
@@ -33,36 +68,5 @@ document.addEventListener('DOMContentLoaded', () => {
         disableRightClick()
       }
     }
-  })
-
-  // Get the gallery grid.
-  const gallery = document.querySelector('.grd-acf-block-grid')
-
-  // If there is no gallery, return.
-  if (!gallery) {
-    return
-  }
-
-  /**
-   * Init Masonry.
-   *
-   * @see https://masonry.desandro.com/options.html
-   */
-  const masonryGrid = new Masonry(gallery, {
-    itemSelector: '.grd-acf-block-image',
-    columnWidth: '.grd-acf-block-grid-sizer',
-    percentPosition: true
-  })
-
-  /**
-   * Init imagesLoaded.
-   *
-   * @see https://imagesloaded.desandro.com/
-   */
-  const imagesLoadedInstance = imagesLoaded(gallery)
-
-  // When images have all loaded, init the Masonry layout.
-  imagesLoadedInstance.on('done', () => {
-    masonryGrid.layout()
   })
 })
