29d106a0ee83624295d83080e53665dc11461bcb
Greg Rickaby <gregrickaby@gmail.com>
2023-07-06 17:35:13 -0500

    preload/preconnect fa

--- themes/insta/header.php
+++ themes/insta/header.php
@@ -18,9 +18,12 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 
-	<link rel="preload" href="//gregrickaby.test/wp-content/uploads/2015/03/cropped-greg-kermit.webp" as="image" type="image/webp">
 	<link rel="preconnect" href="//fonts.googleapis.com">
 	<link rel="preconnect" href="//fonts.gstatic.com" crossorigin>
+	<link rel="preconnect" href="//cdnjs.cloudflare.com" crossorigin>
+	<link rel="preload" href="//gregrickaby.test/wp-content/uploads/2015/03/cropped-greg-kermit.webp" as="image" type="image/webp">
+	<link rel="preload" href="//cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" as="style" type="text/css">
+
 	<?php wp_head(); ?>
 </head>
 
