8a0d651fa7fa3744962a2d6188ca5a3bf13d151c
Greg Rickaby <gregrickaby@gmail.com>
2023-07-06 15:12:51 -0500

    remove author url

--- themes/insta/inc/template-tags.php
+++ themes/insta/inc/template-tags.php
@@ -45,7 +45,7 @@ if ( ! function_exists( 'insta_posted_by' ) ) :
 		$byline = sprintf(
 			/* translators: %s: post author. */
 			esc_html_x( 'by %s', 'post author', 'insta-theme' ),
-			'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
+			'<span class="author vcard">' . esc_html( get_the_author() ) . '</span>'
 		);
 
 		echo '<span class="byline"> ' . $byline . '</span>'; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
