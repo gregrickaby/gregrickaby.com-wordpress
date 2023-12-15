2c59f40eabc4f216fc2c6fd361c5cbac07d6ad12
Greg Rickaby <gregrickaby@gmail.com>
2023-12-14 21:27:58 -0600

    slug based revalidation

--- plugins/nextjs-wordpress-plugin/inc/revalidation.php
+++ plugins/nextjs-wordpress-plugin/inc/revalidation.php
@@ -14,15 +14,16 @@ namespace NEXTJS_WORDPRESS_PLUGIN;
  *
  * @param string $new_status New status.
  * @param string $old_status Old status.
+ * @param object $post       The post object.
  */
-function transition_handler( $new_status, $old_status ): void {
+function transition_handler( $new_status, $old_status, $post ): void {
 	// If the post is a draft, bail.
 	if ( 'draft' === $new_status && 'draft' === $old_status ) {
 		return;
 	}
 
-	// Otherwise, revalidate.
-	on_demand_revalidation();
+	// Otherwise, revalidate..
+	on_demand_revalidation( $post->post_name );
 }
 add_action( 'transition_post_status', __NAMESPACE__ . '\transition_handler', 10, 3 );
 
@@ -32,26 +33,34 @@ add_action( 'transition_post_status', __NAMESPACE__ . '\transition_handler', 10,
  * This function will fire anytime a post is updated.
  * Including: the post status, comments, meta, terms, etc.
  *
- * @usage https://nextjswp.com/api/revalidate
+ * @usage https://nextjswp.com/api/revalidate?slug=foo-bar-baz
  *
  * @see https://nextjs.org/docs/app/building-your-application/data-fetching/fetching-caching-and-revalidating#on-demand-revalidation
+ *
+ * @param string $slug The post slug.
+ *
+ * @return void.
  */
-function on_demand_revalidation(): void {
+function on_demand_revalidation( $slug ): void {
 
 	// Do not run on autosave.
 	if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
 		return;
 	}
 
-	// No constants or Post ID? Bail...
-	if ( ! defined( 'NEXTJS_FRONTEND_URL' ) || ! defined( 'NEXTJS_REVALIDATION_SECRET' ) ) {
+	// No constants or slug? Bail.
+	if ( ! defined( 'NEXTJS_FRONTEND_URL' ) || ! defined( 'NEXTJS_REVALIDATION_SECRET' ) || ! $slug ) {
 		return;
 	}
 
-	// Build the API URL.
-	$revalidation_url = esc_url_raw( NEXTJS_FRONTEND_URL . 'api/revalidate' );
+	// Build the revalidation URL.
+	$revalidation_url = add_query_arg(
+		'slug',
+		$slug,
+		esc_url_raw( rtrim( NEXTJS_FRONTEND_URL, '/' ) . '/api/revalidate' )
+	);
 
-	// Make API request.
+	// POST to the revalidation URL.
 	$response = wp_remote_post(
 		$revalidation_url,
 		[
@@ -64,6 +73,6 @@ function on_demand_revalidation(): void {
 	// Check the response.
 	if ( is_wp_error( $response ) ) {
 		$error_message = $response->get_error_message();
-		error_log( 'Revalidation error: ' . $error_message );
+		error_log( 'Revalidation error: ' . $error_message ); // phpcs:ignore
 	}
 }
