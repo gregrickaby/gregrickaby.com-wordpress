f8bf581ae4bb22b55e579976bcafa86543cd48bd
Greg Rickaby <gregrickaby@gmail.com>
2024-10-26 11:36:06 -0500

    add additional classes. security fixes. other refactors

--- plugins/grd-photo-gallery/src/blocks/gallery/gallery.php
+++ plugins/grd-photo-gallery/src/blocks/gallery/gallery.php
@@ -8,13 +8,11 @@
  * @see https://masonry.desandro.com/
  * @see https://imagesloaded.desandro.com/
  *
- * @param   array $block The block settings and attributes.
- * @param   string $content The block inner HTML (empty).
- * @param   bool $is_preview True during backend preview render.
- * @param   int $post_id The post ID the block is rendering content against.
- *          This is either the post ID currently being displayed inside a query loop,
- *          or the post ID of the post hosting this block.
- * @param   array $context The context provided to the block by the post or it's parent block.
+ * @param array  $block The block settings and attributes.
+ * @param string $content The block inner HTML (empty).
+ * @param bool   $is_preview True during backend preview render.
+ * @param int    $post_id The post ID the block is rendering content against.
+ * @param array  $context The context provided to the block by the post or its parent block.
  *
  * @package Grd\Photo_Gallery
  * @since 1.0.0
@@ -39,65 +37,75 @@ if ( empty( $photos ) ) {
 
 <section id="grd-photo-gallery-gallery" data-testid="grd-photo-gallery">
 	<div class="grd-photo-gallery-grid" data-testid="grd-photo-gallery-grid">
-		<div class="grd-photo-gallery-grid-sizer"></div>
-		<?php
-		// Start counter.
-		$i = 1;
+	<?php
+	$image_index = 1;
 
-		// Loop through photos.
-		foreach ( $photos as $photo ) :
-			$photo_id          = $photo['ID'];
-			$srcset            = wp_get_attachment_image_srcset( $photo_id );
-			$metadata          = wp_get_attachment_metadata( $photo_id );
-			$extended_metadata = get_post_meta( $photo_id, 'extended_image_meta' );
+	// Loop through photos.
+	foreach ( $photos as $photo ) :
+		$photo_id          = $photo['ID'];
+		$srcset            = wp_get_attachment_image_srcset( $photo_id );
+		$metadata          = wp_get_attachment_metadata( $photo_id );
+		$extended_metadata = get_post_meta( $photo_id, 'extended_image_meta' );
 
-			$alt_text   = $photo['alt'] ?? '';
-			$title_text = $photo['title'] ?? '';
-			$caption    = $photo['caption'] ?? '';
+		// Define image attributes, escaping them once at assignment.
+		$alt_text   = esc_attr( $photo['alt'] ?? '' );
+		$title_text = esc_attr( $photo['title'] ?? '' );
+		$caption    = esc_html( $photo['caption'] ?? $title_text ?? $alt_text );
 
-			// If the photo has a caption use it, otherwise, check for the title or alt text.
-			if ( ! $caption ) {
-				$caption = $title_text ? $title_text : $alt_text;
-			}
+		// Get EXIF data, with a fallback to formatted metadata if not present.
+		$exif = ! empty( $extended_metadata['exif_string'] )
+			? esc_html( $extended_metadata['exif_string'] )
+			: Formatting::format_exif_string( $extended_metadata, $metadata );
 
-			// If the photo has an EXIF string use it, otherwise, build one.
-			if ( ! empty( $extended_metadata['exif_string'] ) ) {
-				$exif = $extended_metadata['exif_string'];
-			} else {
-				$exif = Formatting::format_exif_string( $extended_metadata, $metadata );
-			}
-
-			// Build fancybox caption.
-			$fancy_caption = sprintf( '<p>%s</p> <span class="exif">%s</span>', $caption, $exif );
-			?>
-			<figure class="grd-photo-gallery-image" data-image-number="<?php echo esc_attr( $i ); ?>" data-testid="image-<?php echo esc_attr( $i ); ?>"itemprop="image" itemscope itemtype="https://schema.org/ImageObject">
-				<a
-					data-caption="<?php echo esc_attr( $fancy_caption ); ?>"
-					data-fancybox
-					data-slug="<?php echo esc_attr( $photo_id ); ?>"
-					data-testid="image-link"
-					href="<?php echo esc_url( wp_get_original_image_url( $photo_id ) ); ?>"
-				>
-					<?php echo wp_get_attachment_image( $photo_id, 'medium' ); ?>
-				</a>
-				<?php if ( $caption ) : ?>
-					<figcaption class="grd-photo-gallery-image-caption" data-testid="image-caption">
-						<a
-							data-caption="<?php echo esc_attr( $fancy_caption ); ?>"
-							data-slug="<?php echo esc_attr( $photo_id ); ?>"
-							data-testid="image-caption-link"
-							href="<?php echo esc_url( wp_get_original_image_url( $photo_id ) ); ?>"
-							title="<?php echo esc_attr( $caption ); ?>"
-						>
-							<?php echo esc_html( $caption ); ?>
-						</a>
-					</figcaption>
-				<?php endif; ?>
-			</figure>
-			<?php
-			// Increment counter.
-			++$i;
-		endforeach;
+		// Build Fancybox caption.
+		$fancy_caption = sprintf(
+			'<p>%s</p> <span class="exif">%s</span>',
+			wp_kses_post( $caption ),
+			wp_kses_post( $exif )
+		);
 		?>
+		<figure class="grd-photo-gallery-figure" data-image-number="<?php echo esc_attr( $image_index ); ?>" data-testid="image-<?php echo esc_attr( $image_index ); ?>" itemprop="image" itemscope itemtype="https://schema.org/ImageObject">
+			<a
+				aria-label="<?php echo esc_attr( 'View full-size image of ' . $caption ); ?>"
+				class="grd-photo-gallery-image-link"
+				data-caption="<?php echo esc_attr( $fancy_caption ); ?>"
+				data-fancybox
+				data-slug="<?php echo esc_attr( $photo_id ); ?>"
+				data-testid="image-link"
+				href="<?php echo esc_url( wp_get_original_image_url( $photo_id ) ); ?>"
+			>
+				<?php
+				// Output image.
+				echo wp_get_attachment_image(
+					$photo_id,
+					'medium',
+					false,
+					array(
+						'alt'   => $alt_text,
+						'class' => 'grd-photo-gallery-image',
+					)
+				);
+				?>
+			</a>
+			<?php if ( $caption ) : ?>
+				<figcaption class="grd-photo-gallery-image-caption" data-testid="image-caption">
+					<a
+						class="grd-photo-gallery-image-caption-link"
+						data-caption="<?php echo esc_attr( $fancy_caption ); ?>"
+						data-slug="<?php echo esc_attr( $photo_id ); ?>"
+						data-testid="image-caption-link"
+						href="<?php echo esc_url( wp_get_original_image_url( $photo_id ) ); ?>"
+						title="<?php echo esc_attr( $caption ); ?>"
+					>
+						<?php echo esc_html( $caption ); ?>
+					</a>
+				</figcaption>
+			<?php endif; ?>
+		</figure>
+		<?php
+		// Increment counter.
+		++$image_index;
+	endforeach;
+	?>
 	</div>
 </section>
